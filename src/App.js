57f8dc23328c6524e1f55afd9503ef9fcb1c50bd
Semilogo Bamidele <emmanbamidele471@gmail.com>
2022-10-05 23:23:25 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
 import Support from "./components/Support";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Support />
       <AllInOne />
       <Pricing />
+      <Footer />
     </>
   );
 }
